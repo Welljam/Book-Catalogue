d8983b02c0ecadd664c11d61b924f3845a2823bb
William Warwick <warwickw8@gmail.com>
2024-03-27 23:41:33 +0100

    lagt till fler funktioner

--- bookCatalogue.go
+++ bookCatalogue.go
@@ -30,13 +30,19 @@ func main() {
 
 	createBook(db)
 
-	var i, j, k string
-	fmt.Print("Lägg till en bok, ange titel, författare och genre\n")
-	fmt.Scan(&i, &j, &k)
+	fmt.Print("För att lägga till en bok skriv 1\nFör att ta bort en bok skriv 2\nFör att redigera en bok skriv 3\nFör att visa alla böcker skriv 4\n")
 
-	book := newBook{i, j, k}
-	pk := insertBook(db, book)
-	fmt.Printf("ID = %d\n", pk)
+	var answer string
+	fmt.Scan(&answer)
+	if answer == "1" {
+		addBook(db)
+	} else if answer == "2" {
+		deleteBook(db)
+	} else if answer == "3" {
+		changeBook(db)
+	} else if answer == "4" {
+		showBook(db)
+	}
 }
 
 func createBook(db *sql.DB) {
@@ -53,6 +59,27 @@ func createBook(db *sql.DB) {
 	}
 }
 
+func addBook(db *sql.DB) {
+	var userTitle, userAuthor, userGenre string
+	fmt.Print("Lägg till en bok, ange titel, författare och genre\n")
+	fmt.Scan(&userTitle, &userAuthor, &userGenre)
+
+	query := `SELECT title FROM book WHERE title = $1`
+	var title string
+	err := db.QueryRow(query, userTitle).Scan(&title)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			book := newBook{userTitle, userAuthor, userGenre}
+			pk := insertBook(db, book)
+			fmt.Printf("ID = %d\n", pk)
+		} else {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Println("Boken finns redan")
+	}
+}
+
 func insertBook(db *sql.DB, book newBook) int {
 	query := `INSERT INTO book (title, author, genre)
 		VALUES ($1, $2, $3) RETURNING id`
@@ -66,5 +93,89 @@ func insertBook(db *sql.DB, book newBook) int {
 }
 
 func deleteBook(db *sql.DB) {
+	var userDeleteBook string
+	fmt.Print("Ange titeln på boken du vill radera: ")
+	fmt.Scan(&userDeleteBook)
 
+	query := `DELETE FROM book WHERE title = $1`
+
+	result, err := db.Exec(query, userDeleteBook)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if rowsAffected > 0 {
+		fmt.Println("Boken raderades")
+	} else {
+		fmt.Println("Det finns ingen bok med denna titeln")
+	}
+}
+
+func changeBook(db *sql.DB) {
+	var userChangeBook string
+	fmt.Print("Ange titeln på boken du vill redigera: ")
+	fmt.Scan(&userChangeBook)
+
+	query := `SELECT title, author, genre FROM book WHERE title = $1`
+	row := db.QueryRow(query, userChangeBook)
+
+	var (
+		title  string
+		author string
+		genre  string
+	)
+
+	err := row.Scan(&title, &author, &genre)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println("Det finns ingen bok med denna titeln")
+			return
+		}
+		log.Fatal(err)
+	}
+
+	fmt.Print("Ange en ny titel, författare och genre för boken: ")
+	var newTitle, newAuthor, newGenre string
+	fmt.Scan(&newTitle, &newAuthor, &newGenre)
+
+	updateQuery := `UPDATE book SET title = $1, author = $2, genre = $3 WHERE title = $4`
+	_, err = db.Exec(updateQuery, newTitle, newAuthor, newGenre, userChangeBook)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func showBook(db *sql.DB) {
+	query := `SELECT * FROM book`
+
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var (
+			id     int
+			title  string
+			author string
+			genre  string
+		)
+
+		err := rows.Scan(&id, &title, &author, &genre)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Book: %d, Title: %s, Author: %s, Genre: %s\n", id, title, author, genre)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
